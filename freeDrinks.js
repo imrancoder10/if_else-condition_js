a50fed6ba125f5cb2205a21773be26ddc1fe1251
imrancoder10 <imranhossain1148@gmail.com>
2024-08-30 18:07:29 +0600

    Free Drinks js

---
+++ freeDrinks.js
@@ -0,0 +1,13 @@
+/*
+1. Free Drinks
+- Burger more than 500tk: free Coke 
+- Else Coke: 30tk
+*/
+var burger=490;
+
+if(burger>500){
+    console.log('Free cake with burger.');
+}
+else{
+    console.log('you will buy a coke. Coke price is 30tk.')
+}
